3b7f4eb9c8741317db88e5d9bda34f0b3a011dd1
Pieter Jordaan <pieter.jordaan@way2web.nl>
2020-06-25 15:52:46 +0200

    bind api resources in AbstractRestApiServiceProvider

--- src/Providers/AbstractRestApiServiceProvider.php
+++ src/Providers/AbstractRestApiServiceProvider.php
@@ -4,6 +4,8 @@
 namespace W2w\Laravel\Apie\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Psr\Http\Message\ServerRequestInterface;
+use W2w\Laravel\Apie\Services\RequestToFacadeResponseConverter;
 
 /**
  * Service provider that allows you to add a REST api from a service provider.
@@ -16,10 +18,26 @@ abstract class AbstractRestApiServiceProvider extends ServiceProvider
 
     public function register()
     {
+        $apiName = $this->getApiName();
         $apiConfig = $this->getApiConfig();
-        $this->app->get('config')->set('apie.contexts.' . $this->getApiName(), $apiConfig);
+        $this->app->get('config')->set('apie.contexts.' . $apiName, $apiConfig);
         if ($apiConfig['bind-api-resource-facade-response'] ?? true) {
-
+            foreach ($apiConfig['resources'] as $resourceClass) {
+                $this->registerResourceClass($apiName, $resourceClass);
+            }
         }
+    }
+
+    private function registerResourceClass(string $apiName, string $resourceClass)
+    {
+        $this->app->bind($resourceClass, function () use ($resourceClass, $apiName) {
+            /** @var ServerRequestInterface $request */
+            $request = $this->app->get(ServerRequestInterface::class);
+
+            /** @var RequestToFacadeResponseConverter $converter */
+            $converter = $this->app->get(RequestToFacadeResponseConverter::class);
+
+            return $converter->convertRequestToResponse($resourceClass, $request)->getResource();
+        });
     }
 }
