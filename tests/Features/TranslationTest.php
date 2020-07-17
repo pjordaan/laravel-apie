1f209dfe9bb6d35dacc3132d58f2a192723fb76c
Pieter Jordaan <pieter.jordaan@way2web.nl>
2020-07-17 16:30:15 +0200

    Made test always use forwards compatible test

--- tests/Features/TranslationTest.php
+++ tests/Features/TranslationTest.php
@@ -16,6 +16,7 @@ class TranslationTest extends AbstractLaravelTestCase
     {
         $config = $application->make('config');
         $config->set('app.name', __CLASS__);
+        $config->set('apie.use_deprecated_apie_object_normalizer', false);
         $resources = [ApplicationInfo::class, Translation::class];
         $config->set('apie.translations', ['en', 'nl', 'be']);
         $config->set('apie.resources', $resources);
